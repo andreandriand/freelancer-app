e1a49306eddb5f5eb46b5ba77202eba3512edf51
Andre <200411100210@student.trunojoyo.ac.id>
2023-02-25 22:27:31 +0700

    Development Order Backend

--- app/Http/Controllers/Dashboard/MyOrderController.php
+++ app/Http/Controllers/Dashboard/MyOrderController.php
@@ -3,10 +3,32 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdvantageService;
+use App\Models\AdvantageUser;
 use Illuminate\Http\Request;
+
+use App\Models\Order;
+use App\Models\Service;
+use App\Models\User;
+use App\Models\OrderStatus;
+use App\Models\Tagline;
+use App\Models\ThumbnailService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Http\Requests\Dashboard\Order\UpdateOrderRequest;
 
 class MyOrderController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +36,12 @@ class MyOrderController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard.order.index');
+        $order = Order::where('freelancer_id', Auth::user()->id)
+            ->orderBy('created_at', 'desc')
+            ->get();
+        return view('pages.dashboard.order.index', [
+            'order' => $order
+        ]);
     }
 
     /**
@@ -24,7 +51,7 @@ class MyOrderController extends Controller
      */
     public function create()
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -35,7 +62,7 @@ class MyOrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -44,9 +71,22 @@ class MyOrderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Order $order)
     {
-        return view('pages.dashboard.order.detail');
+        $service = Service::where('id', $order->service_id)->first();
+        $advantageService = AdvantageService::where('service_id', $order->service_id)->get();
+        $advantageUser = AdvantageUser::where('service_id', $order->service_id)->get();
+        $tagline = Tagline::where('service_id', $order->service_id)->get();
+        $thumbnail = ThumbnailService::where('service_id', $order->service_id)->get();
+
+        return view('pages.dashboard.order.detail', [
+            'order' => $order,
+            'service' => $service,
+            'advantageService' => $advantageService,
+            'advantageUser' => $advantageUser,
+            'tagline' => $tagline,
+            'thumbnail' => $thumbnail
+        ]);
     }
 
     /**
@@ -55,9 +95,11 @@ class MyOrderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Order $order)
     {
-        return view('pages.dashboard.order.edit');
+        return view('pages.dashboard.order.edit', [
+            'order' => $order
+        ]);
     }
 
     /**
@@ -67,9 +109,24 @@ class MyOrderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateOrderRequest $request, Order $order)
     {
-        //
+        $data = $request->all();
+
+        if (isset($data['file'])) {
+            $data['file'] = $request->file('file')->store(
+                'assets/order/attachment',
+                'public'
+            );
+        }
+
+        $order = Order::find($order->id);
+        $order->file = $data['file'];
+        $order->note = $data['note'];
+        $order->save();
+
+        toast()->success('Order Berhasil Diupdate');
+        return redirect()->route('member.order.index');
     }
 
     /**
@@ -80,17 +137,27 @@ class MyOrderController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return abort(404);
     }
 
 
     public function accepted($id)
     {
-        //
+        $order = Order::find($id);
+        $order->order_status_id = 1;
+        $order->save();
+
+        toast()->success('Order Berhasil Diterima');
+        return back();
     }
 
     public function rejected($id)
     {
-        //
+        $order = Order::find($id);
+        $order->order_status_id = 2;
+        $order->save();
+
+        toast()->success('Order Berhasil Ditolak');
+        return back();
     }
 }
